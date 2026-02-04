97f6b55486d62302ca3f5fb75a1eb42bca1ecc59
rkmjld2 <rkmjld2@gmail.com>
2026-02-04 10:18:07 +0530

    Update app1a_remote.py

--- app1a_remote.py
+++ app1a_remote.py
@@ -13,8 +13,10 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+#from langchain.chains import create_retrieval_chain
+#from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.documents import Document
 
 # Groq integration
